632dfb19888aef63bf2f532a64eb2399a6560c74
vyasaman <66126881+vyasaman@users.noreply.github.com>
2021-01-09 16:04:14 +0530

    Add files via upload

---
+++ ekart/ekart.js
@@ -0,0 +1,47 @@
+function login() {
+    var email = document.getElementById("user_name").value;
+    var pass = document.getElementById("pass").value;
+    if (email == "" || pass == "") {
+
+    }
+    else if (email == "amanvyas720@gmail.com" && pass == "12345") {
+
+        window.location = "index.html";
+    }
+    else {
+        alert("Invalid Email/Password.");
+    }
+    setTimeout('ligin()', 1000);
+}
+function searchBar() {
+    var s = document.getElementsById("search").value;
+    s = s.toLowerCase();
+    if (s == "shoes") {
+
+        window.location = "shoe.html";
+    }
+    else {
+        alert("No Products Found!");
+    }
+}
+function reg() {
+    var pass1 = document.getElementById("pass1").value;
+    var pass2 = document.getElementById("pass2").value;
+    if (pass1 == pass2) {
+        alert("Registration successful.");
+    }
+    else {
+        alert("Passwords Dont Match!")
+    }
+}
+function show() {
+    var p = document.getElementById('pass');
+    if (p.type == "password") {
+        p.type = "text";
+        document.getElementById('sh_image').src = "images/hide.png"
+    }
+    else {
+        p.type = "password";
+        document.getElementById('sh_image').src = "images/show.png"
+    }
+}
